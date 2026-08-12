4ca6c68d927ab0e9ec890d3649ef7b682baccd2f
Yashiro <aldiynsh007@gmail.com>
2026-08-12 15:06:12 +0700

    update student manager

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const _ = require("lodash");
 const data = ["aldi", "budi", "citra", "deni"];
 const hasil = _.chunk(data, 2);
 
+console.log(data);
+console.log("Jadi hasil dari data di atas setelah di chunk data tersebut akan berubah menjadi : ");
 console.log(hasil);
